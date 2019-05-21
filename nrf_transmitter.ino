eb289ba90a373ee4b260abdf3cec057557c55493
purushothaman <30532008+purushothamanpro@users.noreply.github.com>
2019-05-21 18:48:09 +0530

    Update nrf_transmitter.ino

--- nrf_transmitter.ino
+++ nrf_transmitter.ino
@@ -2,10 +2,10 @@
 #include <RF24Network.h>
 #include <SPI.h>
 #include<NewPing.h>
-#define trig_s1 9
-#define echo_s1 8
+#define trig 9
+#define echo 8
 #define max_distance 400
-NewPing s1_sensor(trig_s1,echo_s1,max_distance);
+NewPing sensor(trig,echo,max_distance);
 RF24 radio(40, 53);               // nRF24L01 (CE,CSN)
 RF24Network network(radio);      // Include the radio in the network
 const uint16_t this_node = 00;   // Address of this node in Octal format ( 04,031, etc)
@@ -19,8 +19,8 @@ void setup() {
 void loop() {
   network.update();
   //unsigned long potValue = analogRead(A0);  // Read the potentiometer value
-  unsigned long angleValue = s1_sensor.ping_cm();//map(potValue, 0, 1023, 0, 180); // Convert the value to 0-180
+  unsigned long Value = sensor.ping_cm();//map(potValue, 0, 1023, 0, 180); // Convert the value to 0-180
   RF24NetworkHeader header(node01);     // (Address where the data is going)
-  bool ok = network.write(header, &angleValue, sizeof(angleValue)); // Send the data
-  Serial.println(angleValue);
+  bool ok = network.write(header, &Value, sizeof(Value)); // Send the data
+  Serial.println(Value);
 }
