f249e8100669922535a773cedec7e4afd1068c8f
purushothaman <30532008+purushothamanpro@users.noreply.github.com>
2019-05-21 18:26:32 +0530

    Update nrf_receiver.ino

--- nrf_receiver.ino
+++ nrf_receiver.ino
@@ -6,13 +6,11 @@ RF24 radio(9, 10);               // nRF24L01 (CE,CSN)
 RF24Network network(radio);      // Include the radio in the network
 const uint16_t this_node = 01;   // Address of our node in Octal format ( 04,031, etc)
 const uint16_t node00 = 00;
-//Servo myservo;  // create servo object to control a servo
 void setup() {
   Serial.begin(9600);
   SPI.begin();
   radio.begin();
   network.begin(90, this_node); //(channel, node address)
-  //myservo.attach(3);   // (servo pin)
 }
 void loop() {
   network.update();
@@ -22,7 +20,6 @@ void loop() {
     unsigned long incomingData;
     network.read(header, &incomingData, sizeof(incomingData)); // Read the incoming data
     Serial.println(incomingData);
-    //myservo.write(incomingData);  // tell servo to go to a particular angle
   }
 
 }
